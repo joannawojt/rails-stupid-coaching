b36160230568c5f6cb7350fd2b1d57529d651228
Joanna Wojtowicz <joanna.grazyna.wojtowicz@gmail.com>
2023-05-22 14:28:16 +0200

    Fix rubocop issues

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,3 +13,12 @@ class QuestionsController < ApplicationController
     end
   end
 end
+
+# for future referances another way of writing the code:
+# @answer = if @question == 'I am going to work'
+#             'Great'
+#           elsif @question.slice(-1) == '?'
+#             'Silly question, get dressed and go to work!'
+#           else
+#             "I don't care, get dressed and go to work!"
+#           end
